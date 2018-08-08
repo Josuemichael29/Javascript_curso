2f5d33e547a291943070260f1173c0325767382d
Josue Banda <josuefcfm@gmail.com>
2018-08-08 14:53:52 -0500

    06-Condicionales

--- js/06-condicionales.js
+++ js/06-condicionales.js
@@ -1,3 +1,34 @@
 'use strict'
 
-//Condicional if
+//Condicional IF
+// Si A es igual a B entonces haz algo
+
+var edad = 18;
+var nombre = 'David Suarez';
+
+/*  OPERADORES RACIONALES
+*       Mayor:          >
+*       Menor:          <
+*       Mayor o igual:  >=
+*       Menor o igual:  <=
+*       Igual:          ==
+*       Distinto:       !=
+*/
+
+if(edad >= 18){
+    console.log(nombre + " tiene " + edad + " años, es mayor de edad");
+}else {
+    console.log(nombre + " tiene " + edad + " años, es menor de edad")
+}
+
+/** OPERADORES LÓGICOS
+ * AND(Y):  &&
+ * OR(O):   ||
+ * NEGACIÖN: !
+ */
+
+ // Negación
+ var year = 2016;
+ if(year != 2016){
+     console.log("El año no es 2016")
+ }
