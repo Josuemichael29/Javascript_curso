079d3448e528b2e880add03b4a110e8a1c3b79f8
Josue Banda <josuefcfm@gmail.com>
2018-08-09 11:39:11 -0500

    08-bucle_for

---
+++ js/08-bucle_for.js
@@ -0,0 +1,10 @@
+'use stict'
+
+//  Bucle FOR
+//  Bucle es una estructura de control que se repite varias veces.
+
+for(var i=0; i<10; i++){
+    console.log("El numero es: " + i);
+
+    debugger; //Para pausar en cada iteración y ver las modificaciones que se hicieron-
+}
