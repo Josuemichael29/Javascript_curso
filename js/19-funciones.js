0ff760d805307ff9d89b5da0b2d49ca3e833137b
Josue Banda <josuefcfm@gmail.com>
2018-08-15 15:59:34 -0500

    19-funciones

---
+++ js/19-funciones.js
@@ -0,0 +1,13 @@
+'use strict'
+
+/**
+ * Funciones
+ *      Una función es una agrupación reutilizable de un conjunto de
+ *      instrucciones.
+ */
+
+ function calculadora(){
+     return "Hola soy la calculadora";
+ }
+
+ console.log(calculadora());
