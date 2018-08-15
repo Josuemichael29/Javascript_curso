ce8228abacd279b71b55d748b6cac5434c01151a
Josue Banda <josuefcfm@gmail.com>
2018-08-15 12:13:30 -0500

    12-ejercicio2

--- js/12-ejercicio2.js
+++ js/12-ejercicio2.js
@@ -1,2 +1,29 @@
 'use strict'
 
+/**
+ * Utilizando un buble mostrar le media y la suma de los resultados
+ * introducidos por el usuario hasta que el usuario introduzca un numero 
+ * negativo
+ */
+
+ var suma = 0;
+ var contador = 0;
+ var aux = 0;
+ var ban = true;
+ do{
+    aux = parseInt(prompt('Introduce el numero: ', 0));
+    ban = true;
+    if(isNaN(aux)){
+        aux = 0;
+        contador++;
+    } else if(aux >= 0){
+        suma += aux;
+        contador++;
+    } else{
+        ban = false;
+    }
+ }while(ban == true);
+
+ console.log('La suma es: ' + suma);
+ console.log('La media es: ' + (suma/contador));
+
